eac146d294f5a3b1535660d5e6dc1f9c20b0279c
shunji <shunji0624mm@gmail.com>
2023-02-06 23:18:28 +0900

    task機能の削除(commit漏れ)

--- tm-flont/src/route.ts
+++
@@ -1,9 +0,0 @@
-export const Route = {
-  PROJECT: '/project',
-  PROJECT_GET: '/project/get',
-  PROJECT_DELETE: '/project/delete',
-  PROJECT_UPDATE: '/project/update',
-  TASK_GET: '/task/get',
-  TASK_DELETE: '/task/delete',
-  TASK_CREATE: '/task/create',
-};
